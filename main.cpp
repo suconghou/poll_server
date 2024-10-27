0244da2f7b5a09b715af9b591b34a233a8aad6d6
suconghou <1126045770@qq.com>
2024-10-27 22:12:22 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "poll.cpp"
+
+int main()
+{
+    auto on_loop = [](poll_server &a)
+    {
+        return 5000;
+    };
+
+    auto on_open = [&](poll_server &a, int fd)
+    {
+        printf("client %d connected \n", fd);
+
+        a.write(fd, "hi", [](poll_server &b, int x, int y)
+                { printf("write callback %d %d \n", x, y); });
+    };
+    // n值为0代表对方关闭了,(recv返回0)
+    // -1 代表 收到中断（POLLHUP事件）
+    auto on_data = [](poll_server &a, int fd, const char *buf, int n)
+    {
+        if (n < 1)
+        {
+            // 测试是否重复
+            printf("client %d closed %d \n", fd, n);
+            return;
+        }
+        printf("Received %d data from client: %s\n", n, buf);
+        a.write(fd, buf, n, nullptr);
+    };
+    poll_server a(on_loop, on_open, on_data);
+
+    a.start(8080);
+}
---
+++ poll.cpp
@@ -0,0 +1,305 @@
+#include <ctype.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <set>
+#include <array>
+#include <map>
+#include <vector>
+#include <regex>
+#include <queue>
+#include <functional>
+#include <poll.h>
+#include <fcntl.h>
+
+class Exception : public std::exception
+{
+public:
+    Exception() : pMessage("") {}
+    Exception(const char *pStr) : pMessage(pStr) {}
+    const char *what() const throw() { return pMessage; }
+
+private:
+    const char *pMessage;
+};
+
+class poll_server
+{
+    using self = poll_server;
+
+    struct WriteRequest
+    {
+        std::string data;                               // 要写入的数据
+        std::function<void(self &, int, int)> callback; // 回调函数,参数2:fd，参数3:发送的字节数，负数则为失败
+    };
+
+    struct connection
+    {
+        pollfd info;
+        std::queue<WriteRequest> out;
+    };
+
+private:
+    std::map<int, connection> connections;
+    std::function<int(self &)> OnLoop;
+    std::function<void(self &, int)> OnOpen;
+    std::function<void(self &, int, const char *, int)> OnData;
+    int startup(int port, int backlog = 128, const char *host = "")
+    {
+        int httpd = socket(AF_INET, SOCK_STREAM, 0);
+        if (httpd < 0)
+        {
+            throw Exception(strerror(errno));
+        }
+
+        if (set_nonblocking(httpd) != 0)
+        {
+            throw Exception(strerror(errno));
+        }
+
+        struct sockaddr_in serv_addr;
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(port);
+        serv_addr.sin_addr.s_addr = strlen(host) < 1 ? INADDR_ANY : inet_addr(host);
+        if (bind(httpd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+        {
+            close(httpd);
+            throw Exception(strerror(errno));
+        }
+        if (listen(httpd, backlog) < 0)
+        {
+            close(httpd);
+            throw Exception(strerror(errno));
+        }
+        return httpd;
+    }
+
+    inline int set_nonblocking(int sockfd)
+    {
+        int flags = fcntl(sockfd, F_GETFL, 0);
+        if (flags == -1)
+        {
+            return -1;
+        }
+        flags |= O_NONBLOCK;
+        if (fcntl(sockfd, F_SETFL, flags) == -1)
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+public:
+    poll_server(std::function<int(self &)> on_loop, std::function<void(self &, int)> on_open, std::function<void(self &, int, const char *, int)> on_data)
+        : OnLoop(std::ref(on_loop)),
+          OnOpen(std::ref(on_open)),
+          OnData(std::ref(on_data))
+    {
+    }
+    // 返回值，已经入队的数量，当入队数量过多时，调用者需放缓以防止内存耗尽
+    int write(int fd, const char *data, int len, std::function<void(self &, int, int)> cb)
+    {
+        return write(fd, std::string(data, len), cb);
+    }
+    int write(int fd, std::string data, std::function<void(self &, int, int)> cb)
+    {
+        auto &c = connections.at(fd); // 如果传入的fd不对，此处抛出异常
+        c.out.push({data, cb});
+        c.info.events |= POLLOUT;
+        return c.out.size();
+    }
+
+    bool start(int port, const char *host = "")
+    {
+        int backlog = 128;
+        int server_sock = startup(port, backlog, host);
+        if (server_sock < 1)
+        {
+            return false;
+        }
+
+        connections[server_sock] = {.info = {server_sock, POLLIN, 0}};
+
+        struct sockaddr_in client_name;
+        socklen_t client_name_len = sizeof(client_name);
+        char buf[512]; // recv buffer,全局复用; TODO enlarge
+        while (true)
+        {
+            auto n = OnLoop(*this);
+            if (n < -1) // stop server
+            {
+                break;
+            }
+
+            // 重新组织 pollfd 数组, TODO 此处有性能开销因此连接数也不应过大
+            std::vector<pollfd> pollfds;
+            pollfds.reserve(connections.size()); // 预分配足够的容量
+            for (const auto &c : connections)
+            {
+                pollfds.emplace_back(c.second.info);
+            }
+
+            int num_fds = poll(pollfds.data(), pollfds.size(), n);
+            // 返回值，正整数：就绪的文件描述符数量，0: 超时，-1: 错误，第三个参数配置的是超时时间
+            printf("poll got %d ready\n", num_fds);
+            if (num_fds < 1)
+            {
+                if (num_fds == 0) // 表明超时
+                {
+                    continue;
+                }
+                // 其他情况 poll 返回 -1 代表错误
+                if (errno == EINTR)
+                {
+                    continue;
+                }
+                throw Exception(strerror(errno));
+            }
+
+            for (auto &item : pollfds)
+            {
+                // 检查服务器套接字是否有新连接
+                if (item.fd == server_sock)
+                {
+                    if (item.revents & POLLIN)
+                    {
+                        // 接受新连接
+                        int client_sock = accept(server_sock, (struct sockaddr *)&client_name, &client_name_len);
+                        if (client_sock < 1)
+                        {
+                            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                            {
+                                continue;
+                            }
+                            throw Exception(strerror(errno));
+                        }
+                        if (set_nonblocking(client_sock) != 0)
+                        {
+                            throw Exception(strerror(errno));
+                        }
+                        if (pollfds.size() < backlog)
+                        {
+                            connections[client_sock] = {.info = {client_sock, POLLIN, 0}}; // POLLHUP无需设置，总是会自动报告POLLHUP事件，如果设置了POLLOUT，发送缓冲区一直有空间，会重复报告
+                            OnOpen(*this, client_sock);
+                        }
+                        else
+                        {
+                            // 服务器已满载 TODO log
+                            close(client_sock);
+                            printf("poll server error, server max full \n");
+                        }
+                    }
+                    else if (item.revents != 0)
+                    {
+                        printf("poll server error,waht event %d ? \n", item.revents);
+                    }
+                    // else 没有事件
+
+                    // i==0 是服务器监听的socket，上面是处理逻辑，只需要处理POLLIN事件，处理完毕在此直接跳到下个循环
+                    continue;
+                }
+
+                if (item.revents & POLLIN)
+                {
+                    int ret;
+                    while ((ret = recv(item.fd, buf, sizeof(buf), 0)) > 0)
+                    {
+                        // 成功读取到数据，注意 recv 不会自动添加0结尾，此处我们附加
+                        buf[ret] = '\0';
+                        OnData(*this, item.fd, buf, ret);
+                    }
+                    if (ret == 0)
+                    {
+                        // recv返回值0 代表 对方关闭连接
+                        close(item.fd);
+                        connections.erase(item.fd);
+                        OnData(*this, item.fd, buf, ret);
+                    }
+                    else if (ret == -1)
+                    {
+                        // 判断错误类型
+                        if (errno == EAGAIN || errno == EWOULDBLOCK)
+                        {
+                            // 没有数据可读，继续等待
+                            continue;
+                        }
+                        throw Exception(strerror(errno));
+                    }
+                }
+                else if (item.revents & POLLOUT)
+                {
+                    auto &c = connections.at(item.fd); // 此处必然存在
+                    auto &q = c.out;
+                    if (!q.empty())
+                    {
+                        auto r = q.front();
+                        int bytesSent = send(item.fd, r.data.c_str(), r.data.size(), 0);
+                        if (bytesSent < 0)
+                        {
+                            // 发送失败,回调函数，负数表示失败，可以读取 strerror(errno)
+                            if (r.callback)
+                            {
+                                r.callback(*this, item.fd, bytesSent);
+                            }
+                            // TODO 是否后续会触发close事件？清理资源
+                        }
+                        else if (bytesSent > 0 || bytesSent == r.data.size()) // >=0 (可能r.data.size()==0)
+                        {
+                            // 部分数据发送成功，可能需要稍后再试
+                            r.data.erase(0, bytesSent);
+                            if (r.data.empty())
+                            {
+                                q.pop(); // 发送完成，移除请求
+                                if (r.callback)
+                                {
+                                    r.callback(*this, item.fd, bytesSent);
+                                }
+                            }
+                            else
+                            {
+                                printf("not empty\n");
+                            }
+                        }
+                        else // bytesSent == 0 ， 对方已关闭了链接，此时不在执行callback
+                        {
+                            // TODO test， 是否还会有收到POLLHUP事件？
+                            c.info.events &= ~POLLOUT;
+                            connections.erase(item.fd);
+                        }
+                    }
+                    else
+                    {
+                        // 发送队列为空，移除 POLLOUT 事件
+                        c.info.events &= ~POLLOUT;
+                    }
+                }
+                else if (item.revents & POLLHUP)
+                {
+                    // 对方关闭连接,使用-1标记是POLLHUP触发了
+                    close(item.fd);
+                    connections.erase(item.fd);
+                    OnData(*this, item.fd, nullptr, -1);
+                }
+                else if (item.revents != 0)
+                {
+                    printf("poll error,waht event %d ? \n", item.revents);
+                }
+                // else 没有事件
+            }
+        }
+        close(server_sock);
+        return true;
+    }
+};
